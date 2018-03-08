65397163fa391bfd2ebe6ec744d56a982bf559e4
Loren <miller6413@gmail.com>
2018-03-07 19:15:20 -0500

    add prod gems/

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+
+  root 'epicenter#feed'
+
+  get 'show_user' => 'epicenter#show_user'
+
+  get 'now_following' => 'epicenter#now_following'
+
+  get 'unfollow' => 'epicenter#unfollow'
+
+  get 'tag_tweets' => 'epicenter#tag_tweets'
+
+  get 'all_users' => 'epicenter#all_users'
+
+  get 'followers' => 'epicenter#followers'
+
+  get 'following' => 'epicenter#following'
+
+  resources :tweets
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
